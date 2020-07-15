a0ddd47c965f5ca561fb4f7759809ee1f2558cf9
Levyathanus <michelezenoni1@gmail.com>
2020-07-15 17:25:26 +0200

    L9

---
+++ L9_bitwise_operations.py
@@ -0,0 +1,15 @@
+import cv2, numpy as np
+
+img1 = np.zeros((250,500,3), np.uint8)
+img1 = cv2.rectangle(img1, (200,0), (300,100), (255,255,255), -1)
+img2 = cv2.imread('image_1.png') 
+img2 = cv2.resize(img2, (img1.shape[1],img1.shape[0]))
+
+bit_and = cv2.bitwise_and(img2, img1)
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('bit_and', bit_and)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
